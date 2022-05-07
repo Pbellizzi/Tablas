82f356e80fe86a9717531e1c21e83c8ee433395c
Ballesta44 <103542598+Ballesta44@users.noreply.github.com>
2022-05-06 21:11:17 -0300

    Update tablas.js

--- tablas.js
+++ tablas.js
@@ -5,9 +5,9 @@ let numero = Number(prompt("Elegí una tabla"))
 let fin = Number(10)
 
 function tablaMultiplicar(tabla, hasta) {
+    document.write("<h6>Tabla del " + tabla + "</h6>"); 
     for (var i = 1; i <= hasta; i++)
     document.write("<p>" + tabla + " x " + i + " = ", tabla * i + "<\p>");
    }
 
 tablaMultiplicar(numero,fin)
-
